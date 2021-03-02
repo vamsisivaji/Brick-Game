3e5f304891654aa18c740134847f8d5d8aa8f34e
vamsisivaji <38399482+vamsisivaji@users.noreply.github.com>
2021-03-03 02:03:05 +0530

    Initial commit

---
+++ Assets/Code/BrickSpawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickSpawn : MonoBehaviour
+{
+    public GameObject brick;
+    public float offset;
+    public GameObject[] brickArray;
+    // Start is called before the first frame update
+    float x;
+    float y;
+    void Start()
+    {
+        brickArray = new GameObject[10];
+        x = transform.position.x+1.2f;
+        y = 4.0f;
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < brickArray.Length; i++)
+            {
+                transform.position = new Vector3(x, y, 0);
+                brickArray[i] = Instantiate(brick, transform.position, Quaternion.identity);
+                x = x + 1.2f;
+            }
+            int randValue = Random.Range(0, brickArray.Length);
+            brickArray[randValue].tag = "SuperBrick";
+            x = x - 12.0f;
+            y=y-0.5f;
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Code/PaddleMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float mousepos = Input.mousePosition.x / Screen.width * 16.0f;
+       Vector2 Paddlepos = new Vector2(transform.position.x, transform.position.y);
+       Paddlepos.x = Mathf.Clamp(mousepos, 1, 12);
+       transform.position = Paddlepos;
+        
+    }
+}
